f54eb71342b66e4751eacc6013f3747cfd351fef
AndreyGOIT <ae.borga@gmail.com>
2023-12-12 16:26:24 +0200

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <PageTitle></PageTitle>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageTitle from "./components/PageTitle";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PageTitle,
+  },
+};
 </script>
 
 <style>
